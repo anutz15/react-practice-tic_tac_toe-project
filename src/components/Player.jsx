0cf420ea25c7b67a892c580255c5594786874c96
anutz15 <anujshah120102@gmail.com>
2024-02-07 20:16:56 +0530

    new states and onChange event added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function Player({name,symbol}) {
+export default function Player({initialName,symbol}) {
+  const [playerName,setPlayerName]=useState(initialName);
+  const [isSetting, setIsSetting]=useState(false);
+  function handleEditClick(){
+    // setIsSetting(true);
+    // setIsSetting(isSetting?false:false);
+    // setIsSetting(!isSetting);    // react will schedule the state update and afterwards it will uppdate, not at that instant
+    setIsSetting((editing)=>!editing)  // here working with latest available state value
+    
+  }
+
+  // accept event obj ass parameter
+  // **** remember this concept, to change the text value, use state concept to first have initial value at that place, then onChange event gets trigerred and new value entered which is target value, it renders that, also pass event obj as parameter of that function
+  function handleChange(event){
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName=<span className="player-name">{playerName}</span>;
+  if(isSetting){
+    editablePlayerName=<input type="text" required value={playerName} onChange={handleChange}/>
+    // playerName=<input type="text" required defaultValue={name}/>
+  }
   return (
     <li>
     <span className="player">
-        <span className="player-name">{name}</span>
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
     </span>
-    <button>Edit</button> 
+    <button onClick={handleEditClick}>{isSetting ? "Save":"Edit"}</button> 
     </li>
   )
 }
